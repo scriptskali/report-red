c858acdc84d7839d067e05cfbd989f331e2749a1
Arch0zz <104281088+scriptskali@users.noreply.github.com>
2022-05-28 16:08:27 -0400

    Create report.py

---
+++ report.py
@@ -0,0 +1,73 @@
+BLACK = '\033[30m'
+RED = '\033[31m'
+GREEN = '\033[32m'
+YELLOW = '\033[33m'
+BLUE = '\033[34m'
+MAGENTA = '\033[35m'
+CYAN = '\033[36m'
+WHITE = '\033[37m'
+RESET = '\033[39m'
+import time
+import os
+os.system("clear")
+time.sleep(2)
+print(RED+"▒█▀▀█ █▀▀ ▒█▀▀█ █▀▀█ ▒█▀▀█ ▀▀█▀▀")
+print("▒█▄▄▀ █▀▀ ▒█▄▄█ █░░█ ▒█▄▄▀ ░░█░░")
+print("▒█░▒█ ▀▀▀ ▒█░░░ ▀▀▀▀ ▒█░▒█ ░░▀░░")
+print("###########REPORT RED###########")
+time.sleep(1)
+#menu
+menu = int(input(BLUE+"#1#-INSTAGRAM      #2#-FACEBOOK\n#3#-TIKTOK         #4#-TELEGRAM\n#5#-WHATSAPP       #6#-GITHUB\n[=] OPCION >>>"))
+#informacion
+time.sleep(1)
+username = input("usuario>")
+print(f"ready report ")
+print("Reports ")
+time.sleep(1)
+print("[====      ]{%25}")
+time.sleep(1)
+print("[======    ]{%50}")
+time.sleep(1)
+print("[========  ]{%75}")
+time.sleep(1)
+print("[==========]{%100}")
+
+print("OK REPORT LISTO")
+
+time.sleep(1)
+print(f"air report for dox gmail>{username}@gmail.com")
+time.sleep(1)
+print(f"air report for dox gmail>{username}@email.com")
+time.sleep(1)
+print(f"air report for dox gmail>{username}@hotmail.com")
+time.sleep(1)
+print(f"air report for dox gmail>{username}@tsclip.com")
+time.sleep(1)
+
+
+time.sleep(1)
+print("posible doxxeo")
+time.sleep(1)
+print(f"nick:{username}")
+time.sleep(1)
+print(f"pass:{username}gins111")
+time.sleep(1)
+
+code = input("code pais:")
+num = input(f"ultimos digitos({code})>")
+print(f"scan > {code}{num}")
+print("valid:true")
+print(f"number:{code}{num}")
+print(f"local_format:{num}")
+print(f"international_format:{code}{num}")
+print(f"country_prefix:{code}")
+print("location:    ")
+time.sleep(1)
+print("posible ip net:192.168.64.25")
+time.sleep(1)
+print(RED+"===================")
+time.sleep(1)
+print(GREEN+"LINK:dx.gd.com    ")
+print(RED+"===================")
+time.sleep(1)
+print(MAGENTA+"GRACIAS POR USAR ESTE SCRIPTS")
