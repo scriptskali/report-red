5bf2eee24906ccf5c53cde990f250ad3c5454614
Arch0zz <104281088+scriptskali@users.noreply.github.com>
2022-05-29 13:40:05 -0400

    Update report-install.sh

--- report-install.sh
+++ report-install.sh
@@ -3,33 +3,12 @@
 #created by se4ars3
 #
 
-bold=`echo -en "\e[1m"`
- underline=`echo -en "\e[4m"`
- dim=`echo -en "\e[2m"`
- strickthrough=`echo -en "\e[9m"`
- blink=`echo -en "\e[5m"`
- reverse=`echo -en "\e[7m"`
- hidden=`echo -en "\e[8m"`
+
  normal=`echo -en "\e[0m"`
- black=`echo -en "\e[30m"`
  red=`echo -en "\e[31m"`
  green=`echo -en "\e[32m"`
- orange=`echo -en "\e[33m"`
- blue=`echo -en "\e[34m"`
- purple=`echo -en "\e[35m"`
  aqua=`echo -en "\e[36m"`
- gray=`echo -en "\e[37m"`
- darkgray=`echo -en "\e[90m"`
- lightred=`echo -en "\e[91m"`
- lightgreen=`echo -en "\e[92m"`
- lightyellow=`echo -en "\e[93m"`
- lightblue=`echo -en "\e[94m"`
- lightpurple=`echo -en "\e[95m"`
- lightaqua=`echo -en "\e[96m"`
- white=`echo -en "\e[97m"`
- default=`echo -en "\e[39m"`
-
-
+ 
 clear
 sleep 1
 echo ${red}"▒█▀▀█ █▀▀ ▒█▀▀█ █▀▀█ ▒█▀▀█ ▀▀█▀▀"
